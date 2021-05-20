c588297ad79227576379cf1d462b306ee1310d3a
maxeemes <max.s.99@yandex.ru>
2021-05-21 01:49:18 +0300

    to check

--- Laba2/CSMatrix.h
+++ Laba2/CSMatrix.h
@@ -13,14 +13,14 @@ public:
 	int *GetArrIndRow();
 	int *GetArrIndCol();
 
-	CCSMatrix &operator-=(CCSMatrix &ref);										//операция разности матриц
+	CCSMatrix &operator-=(CCSMatrix &ref);	//операция разности матриц
 private:
-	int Rows = 0;																//количество строк матрицы
-	int Cols = 0;																//количество столбюцов матрицы
-	int Vals = 0;																//количество ненулевых значений в матрице
-	double *ArrVal;																//массив ненулевых значений
-	int *ArrIndRow;																//массив индексов строк
-	int *ArrIndCol = new int[1]{ 0 };											//массив индексации столбцов
+	int Rows = 0;						//количество строк матрицы
+	int Cols = 0;						//количество столбюцов матрицы
+	int Vals = 0;						//количество ненулевых значений в матрице
+	double *ArrVal;						//массив ненулевых значений
+	int *ArrIndRow;						//массив индексов строк
+	int *ArrIndCol = new int[1]{ 0 };	//массив индексации столбцов
 	int SetAllVals(const int InRows, const int InCols, const int NewVals, double * const NewArrVal, int * const NewArrIndRow, int * const NewArrIndCol);	//сохранение готовых значений класса
 	int GetNumValRow(const int IndRow, int *const StartIndRow = 0);																							//получение количества значений в строке
 	int GetNumValCol(const int IndCol, int *const PrevIndCols = 0);																							//получение количества значений в столбце
